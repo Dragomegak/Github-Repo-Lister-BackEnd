eaf59627f7ec37aef8bd58ae41aac2a2dc59d1a5
Dragomegak <stevenle6569@gmail.com>
2020-05-17 12:10:14 -0400

    added other properties

--- server.js
+++ server.js
@@ -9,3 +9,15 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 app.get('/express_backend', (req, res) => {
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 });
+
+app.post('/', (req, res) => {
+    return res.send('Received a POST HTTP method');
+  });
+
+app.put('/', (req, res) => {
+return res.send('Received a PUT HTTP method');
+});
+
+app.delete('/', (req, res) => {
+return res.send('Received a DELETE HTTP method');
+});
